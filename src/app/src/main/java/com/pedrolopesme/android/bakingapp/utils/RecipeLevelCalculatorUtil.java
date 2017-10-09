7dc9c93b037c5a197d516fda201b2da3ab3e3d7b
Pedro Mendes <pedro.mendes@peixeurbano.com>
2017-10-09 19:52:30 -0300

    Changing recipe levels calculator

--- src/app/src/main/java/com/pedrolopesme/android/bakingapp/utils/RecipeLevelCalculatorUtil.java
+++ src/app/src/main/java/com/pedrolopesme/android/bakingapp/utils/RecipeLevelCalculatorUtil.java
@@ -11,16 +11,16 @@ public final class RecipeLevelCalculatorUtil {
     /**
      * Calculates recipe level according to recipe's steps.
      * <p>
-     * Between 1 and 6 : easy; 7 and 10 : medium; greater than 11 : hard
+     * Between 1 and 7 : easy; 8 and 10 : medium; greater than 11 : hard
      *
      * @param recipe
      * @return string resource id
      */
     public static Integer calculate(Recipe recipe) {
         if (recipe != null && recipe.getSteps() != null) {
-            if (recipe.getSteps().size() >= 1 && recipe.getSteps().size() <= 6)
+            if (recipe.getSteps().size() >= 1 && recipe.getSteps().size() <= 7)
                 return R.string.level_easy;
-            else if (recipe.getSteps().size() >= 7 && recipe.getSteps().size() <= 10)
+            else if (recipe.getSteps().size() >= 8 && recipe.getSteps().size() <= 10)
                 return R.string.level_medium;
             else if (recipe.getSteps().size() >= 11)
                 return R.string.level_hard;
