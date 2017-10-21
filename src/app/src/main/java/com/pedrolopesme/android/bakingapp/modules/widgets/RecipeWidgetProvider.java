8f86dd51dabca0a22c4128b8cb6dcc546cbb8b45
Pedro Mendes <pedro.mendes@peixeurbano.com>
2017-10-21 01:13:28 -0200

    Adding basic widget support. Needs improvements.

---
+++ src/app/src/main/java/com/pedrolopesme/android/bakingapp/modules/widgets/RecipeWidgetProvider.java
@@ -0,0 +1,65 @@
+package com.pedrolopesme.android.bakingapp.modules.widgets;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.RemoteViews;
+
+import com.pedrolopesme.android.bakingapp.R;
+import com.pedrolopesme.android.bakingapp.formatters.IngredientsFormatter;
+import com.pedrolopesme.android.bakingapp.models.Recipe;
+import com.pedrolopesme.android.bakingapp.modules.recipes.RecipesActivity;
+
+/**
+ * Implementation of Ingredient Widget functionality.
+ */
+public class RecipeWidgetProvider extends AppWidgetProvider {
+
+    private static final String LOG_TAG = RecipeWidgetProvider.class.getSimpleName();
+
+    public static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                       Recipe recipe, int appWidgetId) {
+
+        Log.i(LOG_TAG, "Running Recipe Widget Provider Update App Widget");
+
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_recipe_provider);
+
+        views.setTextViewText(R.id.tv_recipe_ingredients, new IngredientsFormatter(recipe.getIngredients()).simpleListFormat());
+
+        // Setting up click event
+        Intent intent = new Intent(context, RecipesActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+        views.setOnClickPendingIntent(R.id.rl_recipe_widget, pendingIntent);
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    public static void updateRecipeWidgets(Context context, AppWidgetManager widgetManager, Recipe recipe, int[] widgetIds) {
+        Log.i(LOG_TAG, "Running Recipe Widget Provider updateRecipeWidgets ");
+        for (int widgetId : widgetIds) {
+            updateAppWidget(context, widgetManager, recipe, widgetId);
+        }
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        Log.i(LOG_TAG, "Updating Recipe Recipe Widgets");
+        RecipeWidgetService.startActionUpdateRecipe(context);
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
---
+++ src/app/src/main/java/com/pedrolopesme/android/bakingapp/modules/widgets/RecipeWidgetService.java
@@ -0,0 +1,87 @@
+package com.pedrolopesme.android.bakingapp.modules.widgets;
+
+import android.app.IntentService;
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.pedrolopesme.android.bakingapp.models.Ingredient;
+import com.pedrolopesme.android.bakingapp.models.Recipe;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Ingredients Widget Service
+ */
+public class RecipeWidgetService extends IntentService {
+
+    private static final String LOG_TAG = RecipeWidgetService.class.getSimpleName();
+    public static final String ACTION_UPDATE_RECIPE_WIDGET = "com.pedrolopesme.android.bakingapp.widget.update_recipe";
+
+    public RecipeWidgetService() {
+        super("RecipeWidgetService");
+    }
+
+    /**
+     * Updates widget recipe
+     *
+     * @param context reference
+     */
+    public static void startActionUpdateRecipe(Context context) {
+        Log.d(LOG_TAG, "Running start action update recipe");
+        Intent intent = new Intent(context, RecipeWidgetService.class);
+        intent.setAction(ACTION_UPDATE_RECIPE_WIDGET);
+        context.startService(intent);
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        Log.d(LOG_TAG, "Running on handle intent");
+        if (intent != null) {
+            final String action = intent.getAction();
+            if (action.equals(ACTION_UPDATE_RECIPE_WIDGET)) {
+                handleUpdateRecipeAction();
+            }
+        }
+    }
+
+    /**
+     * Handle Update Recipe Action calls
+     */
+    private void handleUpdateRecipeAction() {
+        Log.d(LOG_TAG, "Hanlding update recipe action");
+        // Do something to retrieve updated recipe
+        Recipe recipe = new Recipe();
+        recipe.setName("Receita teste");
+
+        List<Ingredient> ingredientList = new ArrayList<>();
+        Ingredient i1 = new Ingredient();
+        i1.setIngredient("Ingrediente 1");
+        i1.setMeasure("1 litro");
+        i1.setQuantity(3);
+        ingredientList.add(i1);
+
+        Ingredient i2 = new Ingredient();
+        i2.setIngredient("Ingrediente 2");
+        i2.setMeasure("1 litro");
+        i2.setQuantity(3);
+        ingredientList.add(i2);
+
+        Ingredient i3 = new Ingredient();
+        i3.setIngredient("Ingrediente 2");
+        i3.setMeasure("1 litro");
+        i3.setQuantity(3);
+        ingredientList.add(i3);
+
+        recipe.setIngredients(ingredientList);
+
+        AppWidgetManager widgetManager = AppWidgetManager.getInstance(this);
+        int[] appWidgetIds = widgetManager.getAppWidgetIds(new ComponentName(this, RecipeWidgetProvider.class));
+        RecipeWidgetProvider.updateRecipeWidgets(this, widgetManager, recipe, appWidgetIds);
+    }
+
+}
