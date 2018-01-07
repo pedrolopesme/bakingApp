0407199c8b3d2ac75b55e489c409b46c59a3250d
Pedro Mendes <pedro.mendes@peixeurbano.com>
2018-01-07 17:35:31 -0200

    Fixing tests over Tablets

--- src/app/src/androidTest/java/com/pedrolopesme/android/bakingapp/modules/recipes/RecipesActivityTest.java
+++ src/app/src/androidTest/java/com/pedrolopesme/android/bakingapp/modules/recipes/RecipesActivityTest.java
@@ -1,7 +1,6 @@
 package com.pedrolopesme.android.bakingapp.modules.recipes;
 
 import android.support.test.InstrumentationRegistry;
-import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -13,13 +12,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static com.pedrolopesme.android.bakingapp.matchers.ActionBarMatcher.matchToolbarTitle;
-import static com.pedrolopesme.android.bakingapp.utils.TestUtils.withRecyclerView;
+import static org.hamcrest.CoreMatchers.allOf;
 
 
 @RunWith(AndroidJUnit4.class)
@@ -39,7 +36,7 @@ public class RecipesActivityTest {
         onView(withId(R.id.fl_recipes_container))
                 .check(matches((isDisplayed())));
 
-        onView(withId(R.id.rv_recipes))
+        onView(allOf(withId(R.id.rv_recipes), isDisplayed()))
                 .check(matches((isDisplayed())));
     }
 
